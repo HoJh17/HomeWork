63d4811af7bb839c022b8a58b2b8ecdf8f151f83
Jhou <ckto8575527@gmail.com>
2020-04-18 12:06:04 +0800

    Hw1test

--- Week1/Hw1.test.js
+++ Week1/Hw1.test.js
@@ -1,8 +1,11 @@
-import {stars} from './Hw1'
+import {stars} from './Hw1.js'
 
 describe("hw1", () => {
   it("should return correct answer when n = 1", () => {
-    expect(stars(1)).toEqual(['*'])
+    expect(stars(1)).toEqual(["*"])
+  })
+  it("should return correct answer when n = 4", () => {
+      expect(stars(4)).toEqual(['*',"**","***","****"])
   })
   
 })
